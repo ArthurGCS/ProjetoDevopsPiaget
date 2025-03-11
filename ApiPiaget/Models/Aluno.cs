eb2c056414d275ebcd84ecf6cc5da4599dc15c1e
Jhok1309 <jhonathan.giovanelli@gmail.com>
2025-03-10 21:24:08 -0300

    feat:LogicaGeral

---
+++ ApiPiaget/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace ApiPiaget.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string? Nome { get; set; } 
+        public int RGM { get; set; } 
+        public int CPF { get; set; } 
+       
+       
+
+
+    }
+}
---
+++ ApiPiaget/Models/Professor.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiPiaget.Models
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int RGM { get; set; }
+        public int CPF { get; set; } 
+        public String? Materia { get; set; } 
+        public Aluno? Aluno { get; set; }
+
+    }
+}
